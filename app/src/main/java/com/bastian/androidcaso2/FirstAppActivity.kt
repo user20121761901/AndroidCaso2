87f575b6f27ea5cf6a0c4256b0f1ff20fad73126
Sebastian Mori <mori.sebastian@outlook.com>
2023-08-09 18:26:53 -0500

    creando primer activity

---
+++ app/src/main/java/com/bastian/androidcaso2/FirstAppActivity.kt
@@ -0,0 +1,11 @@
+package com.bastian.androidcaso2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FirstAppActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_first_app)
+    }
+}
