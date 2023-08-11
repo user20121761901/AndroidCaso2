8298e50d52017e33328b4e72870e4a69e1884bd4
Sebastian Mori <mori.sebastian@outlook.com>
2023-08-11 12:47:25 -0500

    se desarrollo el diseño de la aplicacion

--- app/src/main/java/com/bastian/androidcaso2/FirstAppActivity.kt
+++ app/src/main/java/com/bastian/androidcaso2/FirstAppActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class FirstAppActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_first_app)
